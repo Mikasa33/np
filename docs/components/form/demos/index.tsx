1beb94ad18ff1f82087c2092cb59c052f06f2ebb
Mikasa33 <mikasa33@qq.com>
2024-08-07 00:00:17 +0800

    :art: 删除换行

--- docs/components/form/demos/index.tsx
+++ docs/components/form/demos/index.tsx
@@ -6,11 +6,7 @@ import SlotDemo from './Slot.vue'
 import ComponentDemo from './Component.vue'
 
 export const Basic = createComponent(BasicDemo)
-
 export const DefaultValue = createComponent(DefaultValueDemo)
-
 export const Grid = createComponent(GridDemo)
-
 export const Slot = createComponent(SlotDemo)
-
 export const Component = createComponent(ComponentDemo)
